bd693bc3ecf803a4e46e5224ca97189f05c20d52
Granz Scientific <cdg@tuta.io>
2017-04-24 01:21:56 +0000

    Slight changed to pulse width for HV tube supply.  Added geiger_off.py to quickly turn off the HV supply.

---
+++ pi-software/geiger_off.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# This turns off the HV tube supply
+
+import smbus
+
+i2c_addr = 0x19
+
+# I2C config for HV supply
+bus = smbus.SMBus(1) # I2C1 port
+bus.write_byte(i2c_addr, 0) # set pulse-width to zero (off)
+
