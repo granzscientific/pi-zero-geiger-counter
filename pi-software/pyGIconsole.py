7358359114765fa8b3fb4c701c08a4a265779655
Christopher D. Granz <cdg@tuta.io>
2017-07-03 00:27:08 -0400

    Added pyGIconsole.py.

---
+++ pi-software/pyGIconsole.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import logging
+from pyGI.configurator import cfg
+
+#setup logging
+#log = logging.getLogger()
+#log.setLevel(cfg.get('logging','level'))
+#formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+#if cfg.getboolean('logging','write_file'):
+#    filehandler = logging.FileHandler(cfg.get('logging','filename'))
+#    filehandler.setFormatter(formatter)
+#    log.addHandler(filehandler)
+#streamhandler = logging.StreamHandler()
+#streamhandler.setFormatter(formatter)
+#log.addHandler(streamhandler)
+
+if __name__ == "__main__":
+    log.info("Starting pyGIconsole")
+    from pyGI import geigercounter,geigerlog
+    try:
+        #get last totalcount from db
+        #(last_total,last_total_dtc) = geigerlog.get_last_totalcount()
+        #log.info("Last total: %d, total_dtc: %d"%(last_total, last_total_dtc))
+        #start geigercounter
+        #geiger = geigercounter.Geigercounter(total=last_total, total_dtc=last_total_dtc)
+        geiger = geigercounter.Geigercounter()
+        
+        #start geigercounter logging
+        #geigerlog = geigerlog.GeigerLog(geiger)
+    
+        #start the bottle server stuff
+        #geigerserver.start(geiger,geigerlog)
+        last_ticks = geiger.totalcount
+        log.info("Starting ticks update loop")
+        while True:
+            ticks = geiger.totalcount-last_ticks
+            last_ticks = geiger.totalcount
+            log.info("CPS: " + geiger.cps)
+            log.info("CPM: " + geiger.cpm)
+            time.sleep(0.2)
+
+    except KeyboardInterrupt:
+        log.info("Stopping pyGIconsole")
